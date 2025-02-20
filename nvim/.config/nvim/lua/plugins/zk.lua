dd984d119dfd0bf737137ecb1c8df38b61d11184
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-02-20 18:51:47 +1100

    Added template argument to the zk command

--- nvim/.config/nvim/lua/plugins/zk.lua
+++ nvim/.config/nvim/lua/plugins/zk.lua
@@ -19,7 +19,7 @@ return {
       })
 
       local opts = { noremap = true, silent = false }
-      vim.api.nvim_set_keymap("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
+      vim.api.nvim_set_keymap("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: '), template = vim.fn.input('Template: ').md }<CR>", opts)
       vim.api.nvim_set_keymap("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
       vim.api.nvim_set_keymap("n", "<leader>zt", "<Cmd>ZkTags<CR>", opts)
       vim.api.nvim_set_keymap("n", "<leader>zf",
