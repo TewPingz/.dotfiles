af672fdcee205338dcf619738141f58923078cec
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-25 23:28:32 +1100

    Added a formatter and moved lsp actions to snacks picker

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
-if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
-  local function map(...) vim.api.nvim_buf_set_keymap(0, ...) end
-  local opts = { noremap = true, silent = false }
+local function map(...) vim.api.nvim_buf_set_keymap(0, ...) end
+local opts = { noremap = true, silent = false }
 
+if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
   map("n", "<leader>zl", "<Cmd>ZkInsertLink { tags = { 'atomic' } }<CR>", opts)
   map("v", "<leader>zl", ":'<,'>ZkInsertLinkAtSelection { tags = { 'atomic' } }<CR>", opts)
   map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", opts)
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,53 +1,125 @@
 return {
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      bigfile = {
-        enabled = true
-      },
-      dashboard = {
-        enabled = true,
-      },
-      input = {
-        enabled = true
-      },
-      picker = {
-        enabled = true,
-        layout = {
-          preset = "ivy",
-        }
-      },
-      notifier = {
-        enabled = true
-      },
-      quickfile = {
-        enabled = true
-      },
-      scope = {
-        enabled = true
-      },
-      scroll = {
-        enabled = true
-      },
-      statuscolumn = {
-        enabled = true
-      },
-      words = {
-        enabled = true
-      },
-      lazygit = {
-        enabled = true
-      },
-    },
-    keys = {
-      { "<leader><space>", function() Snacks.picker.smart() end,                                   desc = "Smart Find Files" },
-      { "<leader>en",      function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-      { "<leader>ff",      function() Snacks.picker.files() end,                                   desc = "Find Files" },
-      { "<leader>fg",      function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
-      { "<leader>fd",      function() Snacks.picker.grep() end,                                    desc = "Grep" },
-      { "<leader>fr",      function() Snacks.picker.recent() end,                                  desc = "Recent" },
-    },
-  }
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			bigfile = {
+				enabled = true,
+			},
+			dashboard = {
+				enabled = true,
+			},
+			input = {
+				enabled = true,
+			},
+			picker = {
+				enabled = true,
+				layout = {
+					preset = "ivy",
+				},
+			},
+			notifier = {
+				enabled = true,
+			},
+			quickfile = {
+				enabled = true,
+			},
+			scope = {
+				enabled = true,
+			},
+			scroll = {
+				enabled = true,
+			},
+			statuscolumn = {
+				enabled = true,
+			},
+			words = {
+				enabled = true,
+			},
+			lazygit = {
+				enabled = true,
+			},
+		},
+		keys = {
+			{
+				"<leader><space>",
+				function()
+					Snacks.picker.smart()
+				end,
+				desc = "Smart Find Files",
+			},
+			{
+				"<leader>en",
+				function()
+					Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+				end,
+				desc = "Find Config File",
+			},
+			{
+				"<leader>ff",
+				function()
+					Snacks.picker.files({ hidden = true })
+				end,
+				desc = "Find Files",
+			},
+			{
+				"<leader>fg",
+				function()
+					Snacks.picker.git_files()
+				end,
+				desc = "Find Git Files",
+			},
+			{
+				"<leader>fd",
+				function()
+					Snacks.picker.grep()
+				end,
+				desc = "Grep",
+			},
+			{
+				"<leader>fr",
+				function()
+					Snacks.picker.recent()
+				end,
+				desc = "Recent",
+			},
+			{
+				"gd",
+				function()
+					Snacks.picker.lsp_definitions()
+				end,
+				desc = "Goto Definition",
+			},
+			{
+				"gD",
+				function()
+					Snacks.picker.lsp_declarations()
+				end,
+				desc = "Goto Declaration",
+			},
+			{
+				"gr",
+				function()
+					Snacks.picker.lsp_references()
+				end,
+				nowait = true,
+				desc = "References",
+			},
+			{
+				"gI",
+				function()
+					Snacks.picker.lsp_implementations()
+				end,
+				desc = "Goto Implementation",
+			},
+			{
+				"gy",
+				function()
+					Snacks.picker.lsp_type_definitions()
+				end,
+				desc = "Goto T[y]pe Definition",
+			},
+		},
+	},
 }
